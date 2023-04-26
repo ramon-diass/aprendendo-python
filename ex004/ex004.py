4e34decc6944d871f7f6e13ecfe0fd0c2afb64d9
Ramon Dias <123565423+ramon-diass@users.noreply.github.com>
2023-04-26 17:30:27 -0300

    Utilizando o método is para analisar uma variável

---
+++ ex004/ex004.py
@@ -0,0 +1,8 @@
+x = input('Digite algo: ')
+print('É um número? ', x.isnumeric())
+print('É alfabético? ', x.isalpha())
+print('É alfanumérico? ', x.isalnum())
+print('Tem somente espaço? ', x.isspace())
+print('Está somente em maiúsculo? ', x.isupper())
+print('Está somente em minúsculo? ', x.islower())
+print('Está capitalizado? ', x.istitle())
