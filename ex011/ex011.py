d12ed49a7dd78bc6b5e9864e127f3853e2ccf7f4
Ramon Dias <123565423+ramon-diass@users.noreply.github.com>
2023-04-30 11:54:51 -0300

    Cálculo de área

---
+++ ex011/ex011.py
@@ -0,0 +1,5 @@
+m1 = float(input('Digite a metragem da altura da parede: '))
+m2 = float(input('Digite a metragem da largura da parede: '))
+a = m1*m2
+print(f'A área total da parede é de {a:.2f} metros quadrados.')
+print(f'Sabendo que cada litro de tinta pinta uma área de 2 metros quadrados, a quantidade de tinta necessária é de {a/2:.2f} litros.')
