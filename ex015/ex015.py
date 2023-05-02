83ad81d22c38ad99fdc9f8aa9135f834b06c89d7
Ramon Dias <123565423+ramon-diass@users.noreply.github.com>
2023-05-02 19:21:52 -0300

    Programa que calcula o aluguel de um carro a partir de dados de entrada

---
+++ ex015/ex015.py
@@ -0,0 +1,3 @@
+q = float(input('Quantos quilômetros o carro rodou? '))
+d = int(input('Quantos dias o carro ficou alugado? '))
+print(f'O valor a ser pago pelo aluguel do carro é de R${q*0.15+d*60:.2f}.')
