5746ef47ae7304400f3fd1beed3990525e069c8d
Ramon Dias <123565423+ramon-diass@users.noreply.github.com>
2023-05-25 16:58:58 -0300

    Contador regressivo

---
+++ ex046/ex046.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+print('-----\033[7;40m   Contagem Regressiva   \033[m-----')
+sleep(1)
+for cont in range(10, 0, -1):
+    print(cont)
+    sleep(1)
+print('')
+print('💣💣💣')
